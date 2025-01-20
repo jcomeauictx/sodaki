b2e0c4d62669ac422c961f3c976844440c0fda7c
John Comeau <jc@unternet.net>
2025-01-20 11:59:36 -0800

    partial box not showing

--- sodaki.cs
+++ sodaki.cs
@@ -11,6 +11,17 @@
 } bind def
 /grid {
 } bind def
+/box {
+  gsave
+  3 dict begin
+  /boxwidth exch def
+  /radius 10 def
+  /spacing radius 2 div def
+  spacing 0 rmoveto boxwidth spacing sub 0 rlineto  % bottom line
+  currentpoint exch spacing add exch radius 0 90 arc stroke
+  grestore
+  end
+} bind def
 scriptname (sodaki) eq {
   /TimesNewRoman-Bold 80 selectfont
   0 pageheight 80 sub moveto (SODAKI) centershow
@@ -18,8 +29,10 @@ scriptname (sodaki) eq {
   0 -20 rmoveto (A language for systems thinking) centershow
   % draw a grid pagewidth x pagewidth, of black squares with some missing
   % leave 20 pixels at bottom for author name
-  currentpoint 20 sub
-  dup pagewidth dup 2 index min #stack
+  currentpoint 20 sub exch pop  % page height remaining
+  pagewidth min #stack
+  dup .8 mul 4 div exch
+  0 exch #stack rmoveto box
   showpage
 } if
 % vim: tabstop=8 shiftwidth=2 softtabstop=2 syntax=postscr
