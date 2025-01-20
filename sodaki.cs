4f0b324e92323b5796900976a6af4fe089e21526
John Comeau <jc@unternet.net>
2025-01-20 10:36:57 -0800

    pagewidth is limiting in this case

--- sodaki.cs
+++ sodaki.cs
@@ -9,11 +9,17 @@
     currentpoint exch pop moveto show
   grestore
 } bind def
+/grid {
+} bind def
 scriptname (sodaki) eq {
   /TimesNewRoman-Bold 80 selectfont
   0 pageheight 80 sub moveto (SODAKI) centershow
   /TimesNewRoman-Bold 20 selectfont
   0 -20 rmoveto (A language for systems thinking) centershow
+  % draw a grid pagewidth x pagewidth, of black squares with some missing
+  % leave 20 pixels at bottom for author name
+  currentpoint 20 sub
+  dup pagewidth dup 2 index min #stack
   showpage
 } if
 % vim: tabstop=8 shiftwidth=2 softtabstop=2 syntax=postscr
