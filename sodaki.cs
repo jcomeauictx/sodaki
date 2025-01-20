c68041049eb9779bdcdeb1d5fe37d78319e0054c
John Comeau <jc@unternet.net>
2025-01-20 10:24:03 -0800

    title works

--- sodaki.cs
+++ sodaki.cs
@@ -1,11 +1,15 @@
 #!/usr/local/casperscript/bin/cs --
 /centershow {
   gsave
-    dup false charpath [pathbbox] #stack
+    dup false charpath pathbbox pop #stack  % pop yt, we only need xl and xr
+    3 -1 roll sub #stack exch pop  % discard yb
+    pagewidth exch sub 2 div #stack
   grestore
+  currentpoint exch pop moveto show
 } bind def
 scriptname (sodaki) eq {
-  /TimesNewRoman-Bold 40 selectfont
-  0 pageheight 40 sub moveto (SODAKI) centershow
+  /TimesNewRoman-Bold 80 selectfont
+  0 pageheight 80 sub moveto (SODAKI) centershow
+  showpage
 } if
 % vim: tabstop=8 shiftwidth=2 softtabstop=2 syntax=postscr
