6632e7f481c239c8a518a6c69c08f129caa6d08e
John Comeau <jc@unternet.net>
2025-01-20 07:34:28 -0800

    working on book cover

---
+++ sodaki.cs
@@ -0,0 +1,9 @@
+#!/usr/local/casperscript/bin/cs --
+/centershow {
+  dup false charpath #stack
+} bind def
+scriptname (sodaki) eq {
+  /TimesNewRoman-Bold 40 selectfont
+  0 pageheight 40 sub moveto (SODAKI) centershow
+} if
+% vim: tabstop=8 shiftwidth=2 softtabstop=2 syntax=postscr
