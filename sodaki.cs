86013cd91170ac09f4d002ed48b2a836a9eddc4f
John Comeau <jc@unternet.net>
2025-01-20 10:26:18 -0800

    added subtitle

--- sodaki.cs
+++ sodaki.cs
@@ -5,11 +5,15 @@
     3 -1 roll sub #stack exch pop  % discard yb
     pagewidth exch sub 2 div #stack
   grestore
-  currentpoint exch pop moveto show
+  gsave
+    currentpoint exch pop moveto show
+  grestore
 } bind def
 scriptname (sodaki) eq {
   /TimesNewRoman-Bold 80 selectfont
   0 pageheight 80 sub moveto (SODAKI) centershow
+  /TimesNewRoman-Bold 20 selectfont
+  0 -20 rmoveto (A language for systems thinking) centershow
   showpage
 } if
 % vim: tabstop=8 shiftwidth=2 softtabstop=2 syntax=postscr
